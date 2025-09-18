b190ac49503fbf1dd69e8abd9cf51f41747ebd16
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-18 22:12:58 +0200

    adds validation functions

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/MapParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/MapParser.kt
@@ -15,11 +15,11 @@ import de.unisaarland.cs.se.selab.tile.Tile
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.json.boolean
 import kotlinx.serialization.json.int
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import java.io.File
 
 /**
@@ -27,29 +27,38 @@ import java.io.File
  */
 class ValidationException : Exception("Validation Exception, missing or invalid fields")
 
-
-class MapParser (private val simData: SimulationData) {
+/**
+ * Map parser class
+ */
+class MapParser(private val simData: SimulationData) {
     private lateinit var tileIDMap: MutableMap<Int, Tile>
     private lateinit var tileCoordinates: MutableMap<Coordinate, Tile>
 
-
-    fun parse(json: String): Unit {
+    /**
+     * parses the given map config file
+     */
+    fun parse(json: String) {
         val file = File(json)
         val jsonString = file.readText()
         val tiles = Json.parseToJsonElement(jsonString).jsonObject["tiles"]?.jsonArray ?: throw ValidationException()
         parseCreateTiles(tiles)
+        simData.tiles = tileIDMap
+        TODO("add logger")
     }
-
 
     /**
      * iterates over the tiles array extracted from the json file and parses them one by one
      */
-    private fun parseCreateTiles(tiles: JsonArray): Unit {
+    private fun parseCreateTiles(tiles: JsonArray) {
         for (tile in tiles) {
-            parseTile(tile as JsonObject)
+            val parsedTile = parseTile(tile as JsonObject)
+            validateTileShape(parsedTile)
+        }
+
+        for (tile in tileIDMap.values) {
+            validateNeighbors(tile)
         }
     }
-
 
     /**
      * parses a single tile object
@@ -71,13 +80,15 @@ class MapParser (private val simData: SimulationData) {
         parseFarmID(tile, parsedTile)
         parseFarmstead(tile, parsedTile)
         parsePlantableTiles(tile, parsedTile)
-        if (tileIDMap.containsKey(id) || tileCoordinates.containsKey(location)) throw ValidationException()
+        validateUniqueAttributes(id, location)
         tileIDMap[id] = parsedTile
         tileCoordinates[location] = parsedTile
         return parsedTile
     }
 
-
+    /**
+     * parse the airflow and direction for a tile; throw exception if type village
+     */
     private fun parseAirflow(tile: JsonObject, category: TileType): Pair<Boolean, Direction> {
         if (category != TileType.VILLAGE) {
             val airflow = tile["airflow"]?.jsonPrimitive?.boolean ?: throw ValidationException()
@@ -88,22 +99,25 @@ class MapParser (private val simData: SimulationData) {
         throw ValidationException()
     }
 
-
-
+    /**
+     * validate the uniqueness of each new ID and coordinate
+     */
+    private fun validateUniqueAttributes(id: Int, location: Coordinate) {
+        if (tileIDMap.containsKey(id) || tileCoordinates.containsKey(location)) throw ValidationException()
+    }
 
     /**
      * parses the farmID if required by the tile type and updates it in the tile object
      */
     private fun parseFarmID(jsonObject: JsonObject, tile: Tile) {
-        if (tile.category == TileType.FARMSTEAD
-            || tile.category == TileType.PLANTATION
-            || tile.category == TileType.FIELD) {
+        if (tile.category == TileType.FARMSTEAD || tile.category == TileType.PLANTATION ||
+            tile.category == TileType.FIELD
+        ) {
             val farmID = jsonObject["farm"]?.jsonPrimitive?.int ?: throw ValidationException()
             if (farmID < 0) throw ValidationException()
             tile.farmID = farmID
         }
     }
-
 
     /**
      * parses the shed if required by the tile type and updates it in the tile object
@@ -115,14 +129,13 @@ class MapParser (private val simData: SimulationData) {
         }
     }
 
-
     /**
      * parses the plants of the plantation/field tile and updates it in the tile object
      */
     private fun parsePlantableTiles(jsonObject: JsonObject, tile: Tile) {
         if (tile.category == TileType.PLANTATION) {
             val plantType = jsonObject["plant"]?.jsonPrimitive?.content ?: throw ValidationException()
-            if (!PlantType.isPlantationPlant(plantType)) throw ValidationException()
+            validatePlantType(TileType.PLANTATION, plantType)
             var plant: Plant? = null
             when (plantType.uppercase()) {
                 "APPLE" -> plant = Apple()
@@ -134,20 +147,16 @@ class MapParser (private val simData: SimulationData) {
             tile.currentCrop = PlantType.valueOf(plantType.uppercase())
         }
 
-
         if (tile.category == TileType.FIELD) {
             val plants = jsonObject["possiblePlants"]?.jsonArray ?: throw ValidationException()
             val fieldPlants = plants.map { it.jsonPrimitive.content }
             for (element in fieldPlants) {
-                if (!PlantType.isFieldPlant(element)) throw ValidationException()
+                validatePlantType(TileType.FIELD, element)
             }
             tile.possiblePlants = fieldPlants.map { PlantType.valueOf(it.uppercase()) }
             parseCapacity(jsonObject, tile)
         }
-
-
     }
-
 
     /**
      * parses the moisture capacity (tile type check should be done before calling the function)
@@ -160,27 +169,82 @@ class MapParser (private val simData: SimulationData) {
         tile.currentMoisture = capacity
     }
 
-
-
-    private fun validateTileShape(type: TileType): Boolean {
-        // TODO
+    /**
+     * check the shape of the tile based on its type
+     */
+    private fun validateTileShape(tile: Tile) {
+        when (tile.category) {
+            TileType.FARMSTEAD, TileType.MEADOW -> {
+                if (tile.shape != TileShape.SQUARE) throw ValidationException()
+            }
+            TileType.FIELD, TileType.PLANTATION -> {
+                if (tile.shape != TileShape.OCTAGONAL) throw ValidationException()
+            }
+            else -> { return }
+        }
     }
 
-
-    private fun validatePlantType(tileType: TileType, plant: PlantType): Boolean {
-        // TODO
+    /**
+     * validate the correctness of plant types on plantable fields
+     */
+    private fun validatePlantType(tileType: TileType, plant: String) {
+        when (tileType) {
+            TileType.PLANTATION -> { if (!PlantType.isPlantationPlant(plant)) throw ValidationException() }
+            TileType.FIELD -> { if (!PlantType.isFieldPlant(plant)) throw ValidationException() }
+            else -> { return }
+        }
     }
 
-
-    private fun validateAdjacentTiles(): Boolean {
-        // TODO
+    /**
+     * validate tha=e adjoining tiles based on the tile category
+     */
+    private fun validateNeighbors(tile: Tile) {
+        val neighboringTiles = mutableListOf<Tile>()
+        for (direction in Direction.entries) {
+            val adjacent = tile.location.getNeighbor(direction)
+            val neighborTile = tileCoordinates.getOrDefault(adjacent, null)
+            if (neighborTile != null) {
+                neighboringTiles.add(neighborTile)
+            }
+        }
+        when (tile.category) {
+            TileType.FARMSTEAD, TileType.MEADOW -> validateFarmsteadOrMeadowNeighbors(neighboringTiles)
+            TileType.FOREST -> validateForestNeighbors(neighboringTiles)
+            TileType.VILLAGE -> validateVillageNeighbors(neighboringTiles)
+            else -> return
+        }
     }
 
-
-    private fun validateNeighbors(t: Tile): Boolean {
-        // TODO
+    /**
+     * helper function to validate adjoining tiles of a farmstead or a meadow
+     */
+    private fun validateFarmsteadOrMeadowNeighbors(neighbors: List<Tile>) {
+        for (element in neighbors) {
+            if (element.category == TileType.FARMSTEAD || element.category == TileType.MEADOW) {
+                throw ValidationException()
+            }
+        }
     }
 
+    /**
+     * helper function to validate adjoining tiles of a forest
+     */
+    private fun validateForestNeighbors(neighbors: List<Tile>) {
+        for (element in neighbors) {
+            if (element.category == TileType.VILLAGE) {
+                throw ValidationException()
+            }
+        }
+    }
+
+    /**
+     * helper function to validate adjoining tiles of a vilage
+     */
+    private fun validateVillageNeighbors(neighbors: List<Tile>) {
+        for (element in neighbors) {
+            if (element.category == TileType.FOREST) throw ValidationException()
+        }
+    }
 
     /**
      * returns the shape of the tile based on its coordinate
