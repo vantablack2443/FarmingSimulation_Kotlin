0241bcea50441de1e8374bc8ed73272c11d83181
Nehesh Sawarkar <nesa00002@stud.uni-saarland.de>
2025-09-18 13:36:25 +0200

    Map Skeleton

--- src/main/kotlin/de/unisaarland/cs/se/selab/map/Map.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/map/Map.kt
@@ -1,5 +1,42 @@
 package de.unisaarland.cs.se.selab.map
 
-class Map {
+import de.unisaarland.cs.se.selab.coordinate.Coordinate
+import de.unisaarland.cs.se.selab.enumerations.Direction
+import de.unisaarland.cs.se.selab.enumerations.TileType
+import de.unisaarland.cs.se.selab.machine.Machine
+import de.unisaarland.cs.se.selab.tile.Tile
 
+class Map (
+    var tiles: MutableMap<Coordinate, Tile>
+) {
+    fun getTileByCoordinate(coordinate: Coordinate): Tile? {
+        TODO()
+    }
+    fun getTileByID(id: Int): Tile? {
+        TODO()
+    }
+    fun getTilesByRadius(tile:Tile, radius: Int): List<Tile> {
+        TODO()
+    }
+    fun getNeighbor(tile:Tile, direction: Direction): Tile? {
+        TODO()
+    }
+    fun isReachable(machine: Machine, destination: Tile): Boolean {
+        TODO()
+    }
+    fun getReachableTiles(machine: Machine, radius: Int, carryingHarvest: Boolean): List<Tile> {
+        TODO()
+    }
+    fun findTargetShed(machine: Machine, farmSheds: List<Tile>, carryingHarvest: Boolean): Tile? {
+        TODO()
+    }
+    fun getPlantableTiles(): List<Tile> {
+        TODO()
+    }
+    fun filterForPlantable(tiles: List<Tile>): List<Tile> {
+        TODO()
+    }
+    fun filterByType(type: TileType, tiles: List<Tile>): List<Tile> {
+        TODO()
+    }
 }
