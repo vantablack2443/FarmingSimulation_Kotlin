8ff88ef352a78231dadc7b0db5a9815115de7a7c
Kerem <kekx00001@stud.uni-saarland.de>
2025-09-18 13:50:57 +0200

    SimulationData

--- src/main/kotlin/de/unisaarland/cs/se/selab/simulation/SimulationData.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/simulation/SimulationData.kt
@@ -1,4 +1,48 @@
 package de.unisaarland.cs.se.selab.simulation
 
+import de.unisaarland.cs.se.selab.cloud.Cloud
+import de.unisaarland.cs.se.selab.coordinate.Coordinate
+import de.unisaarland.cs.se.selab.farm.Farm
+import de.unisaarland.cs.se.selab.incidents.Incident
+import de.unisaarland.cs.se.selab.machine.Machine
+import de.unisaarland.cs.se.selab.sowingplan.SowingPlan
+import de.unisaarland.cs.se.selab.tile.Tile
+
 class SimulationData {
+    var map: MutableMap<Coordinate, Tile> = mutableMapOf() // take a look at this later
+    var tiles:  MutableMap<Int, Tile> = mutableMapOf()
+    var farms: MutableMap<Int, Farm>  = mutableMapOf() // ID to Farm
+    var machines: MutableMap<Int, Machine> = mutableMapOf()
+    var incidents: MutableMap<Int, Incident> = mutableMapOf()
+    var clouds: MutableMap<Int, Cloud> = mutableMapOf()
+    var sowingPlans: MutableMap<Int, List<SowingPlan>> = mutableMapOf() // mapping tick
+    var sunlightData: Map<Int, Int> = mapOf()// tick to average value
+
+    fun setMap(m: MutableMap<Coordinate, Tile>) {
+        map = m
+    }
+
+    fun setTiles(tiles: MutableMap<Int, Tile>) {
+        this.tiles = tiles
+    }
+
+    fun setFarms(farms: MutableMap<Int, Farm>) {
+        this.farms = farms
+    }
+
+    fun setMachines(machines: MutableMap<Int, Machine>) {
+        this.machines = machines
+    }
+
+    fun setIncidents(incidents: MutableMap<Int, Incident>) {
+        this.incidents = incidents
+    }
+
+    fun setClouds(clouds: MutableMap<Int, Cloud>) {
+        this.clouds = clouds
+    }
+    fun setSowingPlans(plans:  MutableMap<Int, List<SowingPlan>>) {
+        this.sowingPlans = plans
+    }
 }
+
