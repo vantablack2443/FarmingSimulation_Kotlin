00fd691a789c0dc88e1d01044ab876a8c934fdae
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-18 16:44:02 +0200

    completes the skeleton for plants

--- src/main/kotlin/de/unisaarland/cs/se/selab/plant/Cherry.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/plant/Cherry.kt
@@ -1,4 +1,58 @@
-package de.unisaarland.cs.se.selab.plant
 
+package de.unisaarland.cs.se.selab.plant
+import de.unisaarland.cs.se.selab.duration.Duration
+
+const val CHERRY_SUNLIGHT = 120
+const val CHERRY_MOISTURE = 150
+const val CHERRY_HARVEST = 1200000
+const val CHERRY_CUT_START = 21
+const val CHERRY_CUT_END = 22
+const val CHERRY_CUT_START_ALT = 3
+const val CHERRY_CUT_END_ALT = 4
+const val CHERRY_MOW_START_END = 11
+const val CHERRY_HARVEST_START = 13
+const val CHERRY_HARVEST_END = 14
+const val CHERRY_BLOOM_START = 8
+const val CHERRY_BLOOM_END = 9
+
+/**
+ * apple class
+ */
 class Cherry : PlantationPlant() {
+    override var neededSunlight = CHERRY_SUNLIGHT
+    override var neededMoisture = CHERRY_MOISTURE
+    override var harvestEstimate = CHERRY_HARVEST
+    override var cuttingTime = mutableListOf(
+        Pair(Duration(CHERRY_CUT_START, CHERRY_CUT_END), false),
+        Pair(Duration(CHERRY_CUT_START_ALT, CHERRY_CUT_END_ALT), false)
+    )
+    override var mowingTime = mutableListOf(
+        Pair(Duration(CHERRY_MOW_START_END, CHERRY_MOW_START_END), false)
+    )
+    override var harvestingTime = Duration(CHERRY_HARVEST_START, CHERRY_HARVEST_END)
+    override var bloomingTime: Duration? = Duration(CHERRY_BLOOM_START, CHERRY_BLOOM_END)
+
+    override fun animalAttackPenalty() {
+        TODO("Not yet implemented")
+    }
+
+    override fun applyPollinationBuff() {
+        TODO("Not yet implemented")
+    }
+
+    override fun doAnimalAttack() {
+        TODO("Not yet implemented")
+    }
+
+    override fun doBeeHappy() {
+        TODO("Not yet implemented")
+    }
+
+    override fun isBlooming(tick: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun needsHarvesting(tick: Int) {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/de/unisaarland/cs/se/selab/plant/Plant.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/plant/Plant.kt
@@ -1,20 +1,24 @@
 package de.unisaarland.cs.se.selab.plant
 import de.unisaarland.cs.se.selab.duration.Duration
+import de.unisaarland.cs.se.selab.enumerations.ActionType
 
-abstract class Plant(
-    var harvestEstimate : Int,
-    var neededMoisture : Int,
-    var harvestingTime : Duration,
-    var bloomingTime : Duration?,
-    var pollination : Int,
-    var animalAttack : Boolean,
-    var actionsNeeded : MutableList<ActionType>,
-    var lateActions : MutableList<ActionType>
-    ) {
-    abstract fun needsHarvesting(tick : Int) : Unit
-    abstract fun isBlooming(tick : Int) : Boolean
-    abstract fun animalAttackPenalty() : Unit
-    abstract fun doAnimalAttack() : Unit
-    abstract fun doBeeHappy() : Unit
-    abstract fun applyPollinationBuff() : Unit
+/**
+ * abstract plant class
+ */
+abstract class Plant {
+    open var harvestEstimate: Int = -1
+    open var neededMoisture: Int = -1
+    open var neededSunlight: Int = -1
+    open var harvestingTime: Duration = Duration(-1, -1)
+    open var bloomingTime: Duration? = null
+    open var pollination: Int = 0
+    open var animalAttack: Boolean = false
+    open var actionsNeeded: MutableList<ActionType> = mutableListOf()
+    open var lateActions: MutableList<ActionType> = mutableListOf()
+    abstract fun needsHarvesting(tick: Int): Unit
+    abstract fun isBlooming(tick: Int): Boolean
+    abstract fun animalAttackPenalty(): Unit
+    abstract fun doAnimalAttack(): Unit
+    abstract fun doBeeHappy(): Unit
+    abstract fun applyPollinationBuff(): Unit
 }
