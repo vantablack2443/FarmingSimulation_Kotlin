3c7d5aca808cbc1d6a44bf8cc10204b0650b944c
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-19 16:24:43 +0200

    adds skeletons

--- src/main/kotlin/de/unisaarland/cs/se/selab/simulation/Simulation.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/simulation/Simulation.kt
@@ -1,8 +1,11 @@
 package de.unisaarland.cs.se.selab.simulation
 
+import de.unisaarland.cs.se.selab.actionHandlers.ActionPhaseHandler
+import de.unisaarland.cs.se.selab.actionHandlers.IncidentHandler
 import de.unisaarland.cs.se.selab.cloud.Cloud
 import de.unisaarland.cs.se.selab.cloudHandler.CloudHandler
 import de.unisaarland.cs.se.selab.coordinate.Coordinate
+import de.unisaarland.cs.se.selab.harvestestimatehandler.HarvestEstimateHandler
 
 class Simulation (data: SimulationData, var maxTicks: Int, var currentYearTick: Int) {
     var currentTick: Int = 0
@@ -16,5 +19,46 @@ class Simulation (data: SimulationData, var maxTicks: Int, var currentYearTick: 
             mapping[cloud.location] = cloud
         }
     }
+    private var incidentHandler = IncidentHandler(data, this.map)
+    init {
+        var incidents = data.incidents.values.toList()
+        incidentHandler.setIncidents(incidents)
+    }
+    private var harvestEstimator = HarvestEstimateHandler(this.map)
+    private var actionHandler = ActionPhaseHandler(data.farms.values.toList())
+
+    fun run() {
+        TODO()
+    }
+
+    private fun startNextTick(simTick: Int): Boolean {
+        TODO()
+    }
+
+    private fun updateSunlight(currentYear: Int) {
+        TODO()
+    }
+
+    private fun reduceMoisture() {
+        TODO()
+    }
+
+    private fun applyIncidents(yearTick: Int) {
+        TODO()
+    }
+
+    private fun finalStatistics() {
+        TODO()
+    }
+
+    private fun terminate() {
+        TODO()
+    }
+
+    private fun updatePlantationHarvestEstimate() {
+        TODO()
+    }
+
+
 
 }
