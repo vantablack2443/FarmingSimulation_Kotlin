47cc48c41b48b1c6f4c8816cc2122cc48fc3ce91
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-19 16:05:16 +0200

    starts implementation for simulation and cloud handler

--- src/main/kotlin/de/unisaarland/cs/se/selab/cloudHandler/CloudHandler.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/cloudHandler/CloudHandler.kt
@@ -1,10 +1,29 @@
 package de.unisaarland.cs.se.selab.cloudHandler
 
+import de.unisaarland.cs.se.selab.cloud.Cloud
 import de.unisaarland.cs.se.selab.coordinate.Coordinate
 import de.unisaarland.cs.se.selab.map.SimulationMap
-import de.unisaarland.cs.se.selab.cloud.Cloud
 
-class CloudHandler(val simulationMap: SimulationMap, val coordinateToCLoud: MutableMap<Coordinate, Cloud>) {
+/**
+ * handler class for cloud movement
+ */
+class CloudHandler(val simulationMap: SimulationMap) {
     // Add code that will update maxCloudInt
-    var maxCloudInt = 0
+    private lateinit var coordinateToCloud: MutableMap<Coordinate, Cloud>
+    private var maxCloudID = 0
+    private var map = simulationMap
+
+    /**
+     * setter for max cloud ID
+     */
+    fun setMaxCloudID(maxCloudID: Int) {
+        this.maxCloudID = maxCloudID
+    }
+
+    /**
+     * setter for coordinate to cloud mapping
+     */
+    fun setCloudMapping(mapping: MutableMap<Coordinate, Cloud>) {
+        coordinateToCloud = mapping
+    }
 }
--- src/main/kotlin/de/unisaarland/cs/se/selab/simulation/Simulation.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/simulation/Simulation.kt
@@ -1,5 +1,20 @@
 package de.unisaarland.cs.se.selab.simulation
 
-class Simulation (data: SimulationData, maxTicks: Int, currentYearTick: Int){
+import de.unisaarland.cs.se.selab.cloud.Cloud
+import de.unisaarland.cs.se.selab.cloudHandler.CloudHandler
+import de.unisaarland.cs.se.selab.coordinate.Coordinate
+
+class Simulation (data: SimulationData, var maxTicks: Int, var currentYearTick: Int) {
+    var currentTick: Int = 0
+    var map = data.map
+    private var cloudHandler = CloudHandler(map)
+    init {
+        var clouds = data.clouds.values.toList()
+        var mapping = mutableMapOf<Coordinate, Cloud>()
+        cloudHandler.setMaxCloudID(clouds.maxOf { it.id })
+        for (cloud in clouds) {
+            mapping[cloud.location] = cloud
+        }
+    }
 
 }
