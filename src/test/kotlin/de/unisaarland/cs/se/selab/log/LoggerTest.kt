636424cee6dbf9bea937003f98ae4a344962f1e7
Sm1le <keem00001@stud.uni-saarland.de>
2025-09-19 12:45:45 +0200

    logger test fixed

--- src/test/kotlin/de/unisaarland/cs/se/selab/log/LoggerTest.kt
+++ src/test/kotlin/de/unisaarland/cs/se/selab/log/LoggerTest.kt
@@ -11,10 +11,39 @@ class LoggerTest {
     private fun out(): String = buffer.toString().trim()
 
     @Test
-    fun `no debug logs leak`() {
+    fun `IMPORTANT log level tests`() {
+        // no leaks for debug logs
         Logger.setLevel(LogType.IMPORTANT)
         Logger.logSunlight(69, 69)
-        val output = out()
+        var output = out()
         assertEquals("", output, "DEBUG logs should not be printed at IMPORTANT level")
+
+        // no leaks for info logs
+        Logger.setLevel(LogType.IMPORTANT)
+        Logger.logCloudMove(69, 69, 69, 69)
+        output = out()
+        assertEquals("", output, "INFO logs should not be printed at IMPORTANT level")
+
+        // important logs printed
+        Logger.setLevel(LogType.IMPORTANT)
+        Logger.logFarmStart(69)
+        output = out()
+        assertEquals(
+            "[IMPORTANT] Farm: Farm 69 starts its actions.",
+            output,
+            "IMPORTANT logs should be printed at IMPORTANT level"
+        )
+    }
+
+    @Test
+    fun temp() {
+        Logger.setLevel(LogType.DEBUG)
+        Logger.logSunlight(69, 69)
+        var output = out()
+        assertEquals(
+            "[DEBUG] Cloud Movement: On tile 69, the amount of sunlight is 69.",
+            output,
+            "DEBUG logs should not be printed at IMPORTANT level"
+        )
     }
 }
