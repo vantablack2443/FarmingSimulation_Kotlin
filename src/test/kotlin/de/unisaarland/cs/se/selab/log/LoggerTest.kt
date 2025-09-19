753cf7ed5ac2d44ee5dbf3a160b03cbe58455870
Dipu <diba00008@stud.uni-saarland.de>
2025-09-19 11:15:07 +0200

    skeleton logger test

---
+++ src/test/kotlin/de/unisaarland/cs/se/selab/log/LoggerTest.kt
@@ -0,0 +1,12 @@
+package de.unisaarland.cs.se.selab.log
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+
+class LoggerTest {
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+}
