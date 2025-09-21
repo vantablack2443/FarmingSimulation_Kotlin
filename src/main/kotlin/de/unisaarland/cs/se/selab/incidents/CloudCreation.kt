35e5082dee877260d3ce09a3a00c31a793e909ee
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-21 23:19:09 +0200

    implements cloud creation

--- src/main/kotlin/de/unisaarland/cs/se/selab/incidents/CloudCreation.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/incidents/CloudCreation.kt
@@ -1,7 +1,10 @@
 package de.unisaarland.cs.se.selab.incidents
 
+import de.unisaarland.cs.se.selab.cloud.Cloud
 import de.unisaarland.cs.se.selab.cloudHandler.CloudHandler
 import de.unisaarland.cs.se.selab.enumerations.IncidentType
+import de.unisaarland.cs.se.selab.enumerations.TileType
+import de.unisaarland.cs.se.selab.log.Logger
 import de.unisaarland.cs.se.selab.map.SimulationMap
 import de.unisaarland.cs.se.selab.tile.Tile
 
@@ -23,7 +26,46 @@ class CloudCreation(
 ) {
     lateinit var cloudHandler: CloudHandler
 
-    override fun execute(map: SimulationMap, yearTick: Int) {
-        TODO("Not yet implemented")
+    override fun execute(simulationMap: SimulationMap, yearTick: Int) {
+        val affectedTiles = simulationMap.getTilesByRadius(tile, radius)
+            .filter { it.category != TileType.VILLAGE }
+            .sortedBy { it.id }
+        val tileIDs = affectedTiles.map { it.id }
+        Logger.logIncident(this.id, IncidentType.CLOUD_CREATION, tileIDs)
+
+        for (tile in affectedTiles) {
+            val cloud = createCloud(tile)
+            cloudHandler.addCloud(cloud)
+            if (cloudHandler.checkMerge(tile)) {
+                val targetCloud = cloudHandler.getCloudByCoordinate(tile.location) ?: continue
+                val newCloud = cloudHandler.merge(cloud, targetCloud)
+                Logger.logCloudMerge(
+                    targetCloud.id,
+                    cloud.id,
+                    newCloud.id,
+                    this.amount,
+                    this.duration,
+                    tile.id
+                )
+            }
+        }
+    }
+
+    /**
+     * setter for cloud handler
+     */
+    fun setCloudHandler(cloudHandler: CloudHandler) {
+        this.cloudHandler = cloudHandler
+    }
+
+    /**
+     * creates a new cloud instance and adds it into cloud handler mapping
+     */
+    private fun createCloud(tile: Tile): Cloud {
+        val newID = cloudHandler.getMaxCloudID()
+        cloudHandler.setMaxCloudID(newID + 1)
+        val newCloud = Cloud(newID, tile.location, this.duration, this.amount)
+        cloudHandler.addCloud(newCloud)
+        return newCloud
     }
 }
