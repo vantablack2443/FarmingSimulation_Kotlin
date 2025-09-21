651329053df6d4679cb527a95cbd322349b5eaa7
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-21 14:48:11 +0200

    adds getters

--- src/main/kotlin/de/unisaarland/cs/se/selab/machine/PlantAndHarvest.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/machine/PlantAndHarvest.kt
@@ -2,5 +2,21 @@ package de.unisaarland.cs.se.selab.machine
 
 import de.unisaarland.cs.se.selab.enumerations.PlantType
 
-class PlantAndHarvest(var plant: PlantType, var harvestAmount: Int) {
+/**
+ * custom class for a pair of plant type and harvested amount
+ */
+data class PlantAndHarvest(var plant: PlantType, var harvestAmount: Int) {
+    /**
+     * returns the plant type
+     */
+    fun getPlant(): PlantType {
+        return plant
+    }
+
+    /**
+     * returns the harvested amount
+     */
+    fun getAmount(): Int {
+        return harvestAmount
+    }
 }
