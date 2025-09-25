1552cb8c12308dde995c68d8d5dee993ad0bed1b
Ana Shikhiashvili <ansh00004@stud.uni-saarland.de>
2025-09-25 18:59:49 +0200

    adds a validation test

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/SystemTestRegistration.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/SystemTestRegistration.kt
@@ -7,6 +7,7 @@ import de.unisaarland.cs.se.selab.systemtest.selab25.basictests.FarmNoPlantableT
 import de.unisaarland.cs.se.selab.systemtest.selab25.basictests.MachineDefaultAction
 import de.unisaarland.cs.se.selab.systemtest.selab25.basictests.MachineWrongShed
 import de.unisaarland.cs.se.selab.systemtest.selab25.basictests.SowingPlanFieldsDifferentFarms
+import de.unisaarland.cs.se.selab.systemtest.selab25.basictests.ValidateMachineActionPlantTrue
 
 /**
  * Used for test registration
@@ -25,6 +26,7 @@ object SystemTestRegistration {
         testSuite.registerTest(FarmNoPlantableTiles())
         testSuite.registerTest(MachineWrongShed())
         testSuite.registerTest(MachineDefaultAction())
+        testSuite.registerTest(ValidateMachineActionPlantTrue())
     }
 
     /**
