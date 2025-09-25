f885e65befe27619f40dbdc0e0ddbb95dc75427c
Dipu <diba00008@stud.uni-saarland.de>
2025-09-26 01:59:00 +0200

    farmParser basic test

---
+++ src/test/kotlin/de/unisaarland/cs/se/selab/parser/FarmParserTest.kt
@@ -0,0 +1,22 @@
+package de.unisaarland.cs.se.selab.parser
+
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class FarmParserTest {
+
+    @Test
+    fun testParserWithSimpleConfigs() {
+        val mapJsonPath = "src/systemtest/resources/farmParserTest/validMap.json"
+        val farmJsonPath = "src/systemtest/resources/farmParserTest/invalidFarm.json"
+        val scenarioJsonPath = "src/systemtest/resources/farmParserTest/validScenario.json"
+
+        val parser = Parser()
+//        val result = parser.parse(listOf(mapJsonPath, farmJsonPath, scenarioJsonPath))
+//        assertNotNull(result, "Parsing returned null SimulationData")
+
+        assertFailsWith<Exception> {
+            parser.parse(listOf(mapJsonPath, farmJsonPath, scenarioJsonPath))
+        }
+    }
+}
