3a88517e2b2012330af258c707c2140cbbe3f3aa
Nehesh Sawarkar <nesa00002@stud.uni-saarland.de>
2025-09-26 15:16:17 +0200

    merge conflict

---
+++ src/test/kotlin/de/unisaarland/cs/se/selab/parser/MapParserTests.kt
@@ -0,0 +1,115 @@
+package de.unisaarland.cs.se.selab.parser
+//
+// import de.unisaarland.cs.se.selab.enumerations.TileShape
+// import de.unisaarland.cs.se.selab.enumerations.TileType
+// import de.unisaarland.cs.se.selab.simulation.SimulationData
+// import org.junit.jupiter.api.Assertions
+// import org.junit.jupiter.api.Test
+// import java.io.File
+// import kotlin.test.assertEquals
+//
+// class MapParserTests {
+//
+//    @Test
+//    fun testSimpleMapParsing() {
+//        val jsonString = """
+//    {
+//      "tiles": [
+//        {
+//          "id": 0,
+//          "coordinates": { "x": 1, "y": 1 },
+//          "category": "FARMSTEAD",
+//          "airflow": false,
+//          "farm": 0,
+//          "shed": true
+//        }
+//      ]
+//    }
+//    """
+//        val tempFile = File.createTempFile("testmap", ".json")
+//        tempFile.writeText(jsonString)
+//        val simData = SimulationData()
+//        val parser = MapParser(simData)
+//        parser.parse(tempFile.absolutePath)
+//        tempFile.deleteOnExit()
+//        val map = simData.map
+//
+//        val farmstead = map.getTileByID(0)
+//        assertEquals(1, farmstead?.location?.x)
+//        assertEquals(1, farmstead?.location?.y)
+//        assertEquals(TileType.FARMSTEAD, farmstead?.category)
+//        assertEquals(0, farmstead?.farmID)
+//        assertEquals(true, farmstead?.shed)
+//        assertEquals(false, farmstead?.airflow)
+//    }
+//
+//    @Test
+//    fun testDontGrowOnSquare() {
+//        val jsonString = """
+//    {
+//      "tiles": [
+//        {
+//          "id": 0,
+//          "coordinates": { "x": 1, "y": 1 },
+//          "category": "FARMSTEAD",
+//          "farm": 0,
+//          "shed": true,
+//          "airflow": false
+//        },
+//        {
+//            "id": 1,
+//      "coordinates": { "x": 2, "y": 2 },
+//      "category": "FIELD",
+//      "farm": 0,
+//      "capacity": 10000,
+//      "possiblePlants": ["PUMPKIN", "WHEAT"],
+//      "airflow": false
+//            }
+//      ]
+//    }
+//    """
+//        val tempFile = File.createTempFile("testmap", ".json")
+//        tempFile.writeText(jsonString)
+//        val simData = SimulationData()
+//        val parser = MapParser(simData)
+//        parser.parse(tempFile.absolutePath)
+//        tempFile.deleteOnExit()
+//        val map = simData.map
+//
+//        val farmstead = map.getTileByID(0)
+//        val field = map.getTileByID(1)
+//        Assertions.assertFalse(farmstead?.shape == TileShape.OCTAGONAL)
+//        Assertions.assertFalse(field?.shape == TileShape.SQUARE)
+//    }
+//
+//    @Test
+//    fun testNoWildlifeNearMyCity() {
+//        val jsonString = """
+//    {
+//      "tiles": [
+//        {
+//          "id": 0,
+//          "coordinates": { "x": 1, "y": 1 },
+//          "category": "VILLAGE"
+//        },
+//        {
+//          "id": 1,
+//          "coordinates": { "x": 2, "y": 2 },
+//          "category": "FOREST",
+//          "airflow": false
+//        }
+//      ]
+//    }
+//    """
+//        val tempFile = File.createTempFile("testmap", ".json")
+//        tempFile.writeText(jsonString)
+//        val simData = SimulationData()
+//        val parser = MapParser(simData)
+//
+//        Assertions.assertThrows(ValidationException::class.java) {
+//            parser.parse(tempFile.absolutePath)
+//        }
+//
+//        tempFile.deleteOnExit()
+//    }
+// }
