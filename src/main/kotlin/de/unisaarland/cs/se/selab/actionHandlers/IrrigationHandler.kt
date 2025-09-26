f5bc8911f56f9fc593ca0c3b1c5e032aac65f08d
Nehesh Sawarkar <nesa00002@stud.uni-saarland.de>
2025-09-26 23:53:43 +0200

    empty check before minby

--- src/main/kotlin/de/unisaarland/cs/se/selab/actionHandlers/IrrigationHandler.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/actionHandlers/IrrigationHandler.kt
@@ -36,7 +36,8 @@ class IrrigationHandler(
         }
 
         // get target tile for first action
-        val targetTile = findTargetTile(machine, operableTiles)
+        val targetTile = findTargetTile(machine, operableTiles) ?: return
+
         // perform action on target tile
         performAction(machine, targetTile)
         // add tile to farm's tileHashMap so that it won't
@@ -106,10 +107,10 @@ class IrrigationHandler(
     /**
      * Finds the target tile with the lowest ID that is reachable by the machine from the list of operable tiles.
      */
-    private fun findTargetTile(m: Machine, operableTiles: List<Tile>): Tile {
-        return operableTiles
-            .filter { tile -> simulationMap.isReachable(m, tile) }
-            .minBy { it.id }
+    private fun findTargetTile(m: Machine, operableTiles: List<Tile>): Tile? {
+        operableTiles.filter { tile -> simulationMap.isReachable(m, tile) }
+        if (operableTiles.isEmpty()) return null
+        return operableTiles.minBy { it.id }
     }
 
     /**
