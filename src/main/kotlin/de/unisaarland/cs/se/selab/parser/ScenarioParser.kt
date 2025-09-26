3f67e697a0eee086b7e837ade003f96066dd46a9
Nehesh Sawarkar <nesa00002@stud.uni-saarland.de>
2025-09-26 14:20:42 +0200

    fixed parser (Hopefully)

--- src/main/kotlin/de/unisaarland/cs/se/selab/parser/ScenarioParser.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/parser/ScenarioParser.kt
@@ -404,7 +404,7 @@ class ScenarioParser(private val simData: SimulationData) {
             val newVillagesTiles = tiles.intersect(newVillages).toList()
 
             if (tiles.none { it.category == TileType.VILLAGE } &&
-                newVillagesTiles.isNotEmpty()
+                newVillagesTiles.isEmpty()
             ) {
                 return false
             }
