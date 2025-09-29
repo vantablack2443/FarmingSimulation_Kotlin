a81a8a7f5280876414f61fb08b95e8cbfa392d8a
Sm1le <keem00001@stud.uni-saarland.de>
2025-09-29 11:40:48 +0200

    added sowing (simplePlan) systemTest

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/SowingPlanSimplePlan.kt
@@ -0,0 +1,29 @@
+package de.unisaarland.cs.se.selab.systemtest.selab25.basictests
+
+import de.unisaarland.cs.se.selab.systemtest.selab25.utils.ExampleSystemTestExtension
+
+/**
+ * test simple action of sowing on a small map
+ */
+class SowingPlanSimplePlan : ExampleSystemTestExtension() {
+    override val name = "Sowing Plan Fields"
+    override val description = "Tests sowing plan parsing"
+
+    // Paths are relative from the `src/systemtest/resources` directory.
+    override val farms = "sowing_basic/farmsSimplePlan.json"
+    override val scenario = "sowing_basic/scenarioSimplePlan.json"
+    override val map = "sowing_basic/mapSimplePlan.json"
+
+    override val logLevel = "DEBUG"
+    override val maxTicks = 1
+    override val startYearTick = 19
+
+    override suspend fun run() {
+        skipUntilString("[DEBUG] Farm 0 has the following active sowing plans it intends pursue in this tick: [0]")
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 0 for 4 days")
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 1 for 4 days")
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 3 for 4 days")
+        assertNextLine("[IMPORTANT] Farm Sowing: Machine 0 has sowed WHEAT according to sowing plan 0")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished and returns to thee shed at 2")
+    }
+}
