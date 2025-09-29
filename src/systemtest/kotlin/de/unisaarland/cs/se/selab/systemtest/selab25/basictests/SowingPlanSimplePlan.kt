3c9f4ff7b5664a93103fffe58f30e2f3f5cc8b79
Sm1le <keem00001@stud.uni-saarland.de>
2025-09-29 12:03:47 +0200

    fixed sowingPlanSimplePlan

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/SowingPlanSimplePlan.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/SowingPlanSimplePlan.kt
@@ -1,6 +1,9 @@
 package de.unisaarland.cs.se.selab.systemtest.selab25.basictests
 
+import de.unisaarland.cs.se.selab.systemtest.api.SystemTestAssertionError
 import de.unisaarland.cs.se.selab.systemtest.selab25.utils.ExampleSystemTestExtension
+
+const val SOWING_PLAN_EXECUTED = "[IMPORTANT] Farm Sowing: Machine 0 has sowed WHEAT according to sowing plan 0."
 
 /**
  * test simple action of sowing on a small map
@@ -19,11 +22,27 @@ class SowingPlanSimplePlan : ExampleSystemTestExtension() {
     override val startYearTick = 19
 
     override suspend fun run() {
-        skipUntilString("[DEBUG] Farm 0 has the following active sowing plans it intends pursue in this tick: [0]")
-        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 0 for 4 days")
-        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 1 for 4 days")
-        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 3 for 4 days")
-        assertNextLine("[IMPORTANT] Farm Sowing: Machine 0 has sowed WHEAT according to sowing plan 0")
-        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished and returns to thee shed at 2")
+        skipUntilString(
+            "[DEBUG] Farm: Farm 0 has the following active sowing plans" +
+                " it intends to pursue in this tick: 0."
+        )
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 0 for 4 days.")
+        assertNextLine(SOWING_PLAN_EXECUTED)
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 1 for 4 days.")
+        assertNextLine(SOWING_PLAN_EXECUTED)
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs SOWING on tile 3 for 4 days.")
+        assertNextLine(SOWING_PLAN_EXECUTED)
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished and returns to the shed at 2.")
+        assertNextLine("[IMPORTANT] Farm: Farm 0 finished its actions.")
+    }
+
+    private suspend fun skipUntilString(startString: String): String {
+        val line: String = getNextLine()
+            ?: throw SystemTestAssertionError("End of log reached when there should be more.")
+        return if (line.startsWith(startString)) {
+            line
+        } else {
+            skipUntilString(startString)
+        }
     }
 }
