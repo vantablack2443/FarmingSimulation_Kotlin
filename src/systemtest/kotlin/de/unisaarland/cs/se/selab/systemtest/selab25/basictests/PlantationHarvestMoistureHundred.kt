57715f7671e97b2797a21572c8575ef5a8b1f4d0
Theijitha Kulunuguna <loku00001@stud.uni-saarland.de>
2025-09-29 16:29:09 +0200

    Fixes PlantationHarvestMoistureSeventy.kt and PlantationHarvestMoistureHundred.kt

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/PlantationHarvestMoistureHundred.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/PlantationHarvestMoistureHundred.kt
@@ -1,8 +1,6 @@
 package de.unisaarland.cs.se.selab.systemtest.selab25.basictests
 
 import de.unisaarland.cs.se.selab.systemtest.selab25.utils.ExampleSystemTestExtension
-import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogLevel
-import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogType
 
 /**
  * Checks if after harvesting plantation tiles lose moisture at 100L/tick
@@ -33,13 +31,34 @@ class PlantationHarvestMoistureHundred : ExampleSystemTestExtension() {
         assertNextLine("[INFO] Simulation Info: Simulation started at tick 19 within the year.")
         assertNextLine("[INFO] Simulation Info: Tick 0 started at tick 19 within the year.")
 
-        val expectedLine = "[INFO] Simulation Info: Tick 1 started at tick 20 within the year."
-        assert(skipUntilLogType(LogLevel.INFO, LogType.SIMULATION_INFO) == expectedLine)
+        assertNextLine("[INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 0 PLANTATION tiles.")
+
+        assertNextLine("[IMPORTANT] Farm: Farm 0 starts its actions.")
+        assertNextLine(
+            "[DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: ."
+        )
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs HARVESTING on tile 1 for 4 days.")
+        assertNextLine("[IMPORTANT] Farm Harvest: Machine 0 has collected 800000 g of ALMOND harvest.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished and returns to the shed at 0.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 unloads 800000 g of ALMOND harvest in the shed.")
+        assertNextLine("[IMPORTANT] Farm: Farm 0 finished its actions.")
+        assertNextLine("[INFO] Harvest Estimate: Harvest estimate on tile 1 changed to 0 g of ALMOND.")
+        assertNextLine("[INFO] Simulation Info: Tick 1 started at tick 20 within the year.")
 
         assertNextLine("[INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 0 PLANTATION tiles.")
 
-        val expectedLine2 = "[INFO] Simulation Info: Tick 1 started at tick 21 within the year."
-        assert(skipUntilLogType(LogLevel.INFO, LogType.SIMULATION_INFO) == expectedLine2)
+        assertNextLine("[IMPORTANT] Farm: Farm 0 starts its actions.")
+        assertNextLine(
+            "[DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: ."
+        )
+        assertNextLine("[IMPORTANT] Farm Action: Machine 0 performs HARVESTING on tile 1 for 4 days.")
+        assertNextLine("[IMPORTANT] Farm Harvest: Machine 0 has collected 0 g of ALMOND harvest.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 is finished and returns to the shed at 0.")
+        assertNextLine("[IMPORTANT] Farm Machine: Machine 0 unloads 0 g of ALMOND harvest in the shed.")
+        assertNextLine("[IMPORTANT] Farm: Farm 0 finished its actions.")
+        assertNextLine("[DEBUG] Harvest Estimate: Required actions on tile 1 were not performed: IRRIGATING.")
+        assertNextLine("[INFO] Harvest Estimate: Harvest estimate on tile 1 changed to 0 g of ALMOND.")
+        assertNextLine("[INFO] Simulation Info: Tick 2 started at tick 21 within the year.")
 
         assertNextLine("[INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 1 PLANTATION tiles.")
     }
