66526024f293618c19d95ab1389346b5c10efece
Kerem <kekx00001@stud.uni-saarland.de>
2025-09-30 16:34:08 +0200

    IncidentSpaceBetweenTiles.kt system test

---
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/IncidentSpaceBetweenTiles.kt
@@ -0,0 +1,54 @@
+package de.unisaarland.cs.se.selab.systemtest.selab25.basictests
+
+import de.unisaarland.cs.se.selab.systemtest.selab25.utils.ExampleSystemTestExtension
+
+/**
+ * Test for an incident with space between tiles
+ */
+class IncidentSpaceBetweenTiles : ExampleSystemTestExtension() {
+    override val name = "IncidentSpaceBetweenTiles"
+    override val description = "Tests that an incident with space between tiles works correctly."
+
+    // Paths are relative from the `src/systemtest/resources` directory.
+    override val farms = "incidentSpace/farms.json"
+    override val scenario = "incidentSpace/scenario.json"
+    override val map = "incidentSpace/map.json"
+
+    override val logLevel = "DEBUG"
+    override val maxTicks = 1
+    override val startYearTick = 8
+
+    val hi = """
+[INFO] Initialization Info: map.json successfully parsed and validated.
+[INFO] Initialization Info: farms.json successfully parsed and validated.
+[INFO] Initialization Info: scenario.json successfully parsed and validated.
+[INFO] Simulation Info: Simulation started at tick 8 within the year.
+[INFO] Simulation Info: Tick 0 started at tick 8 within the year.
+[INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 0 PLANTATION tiles.
+[IMPORTANT] Farm: Farm 0 starts its actions.
+[DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: .
+[IMPORTANT] Farm: Farm 0 finished its actions.
+[IMPORTANT] Incident: Incident 0 of type BEE_HAPPY happened and affected tiles 2.
+[INFO] Harvest Estimate: Harvest estimate on tile 2 changed to 1260000 g of CHERRY.
+[IMPORTANT] Simulation Info: Simulation ended at tick 1.
+[IMPORTANT] Simulation Info: Simulation statistics are calculated.
+[IMPORTANT] Simulation Statistics: Farm 0 collected 0 g of harvest.
+[IMPORTANT] Simulation Statistics: Total amount of POTATO harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of WHEAT harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of OAT harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of PUMPKIN harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of APPLE harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of GRAPE harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of ALMOND harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of CHERRY harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total harvest estimate still in fields and plantations: 1260000 g.
+    """.trimIndent()
+
+    override suspend fun run() {
+        val lineIterator = hi.lines().iterator()
+        while (lineIterator.hasNext()) {
+            val currentLine = lineIterator.next()
+            assertNextLine(currentLine)
+        }
+    }
+}
