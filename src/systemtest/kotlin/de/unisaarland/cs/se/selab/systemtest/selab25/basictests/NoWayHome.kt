7e654ff613db04740051a267cfa76c585cf36be7
Sm1le <keem00001@stud.uni-saarland.de>
2025-10-01 15:05:22 +0200

    added broken machine incident on machine returning tests

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/NoWayHome.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/NoWayHome.kt
@@ -24,6 +24,8 @@ class NoWayHome : ExampleSystemTestExtension() {
         assertNextLine("[IMPORTANT] Farm Action: Machine 1 performs HARVESTING on tile 1 for 4 days.")
         assertNextLine("[IMPORTANT] Farm Harvest: Machine 1 has collected 1083000 g of GRAPE harvest.")
         assertNextLine("[IMPORTANT] Farm Machine: Machine 1 is finished but failed to return.")
+        assertNextLine("[IMPORTANT] Farm: Farm 1 finished its actions.")
+        assertNextLine("[IMPORTANT] Incident: Incident 0 of type BROKEN_MACHINE happened and affected tiles 1.")
         skipUntilString("[IMPORTANT] Simulation Info: Simulation statistics are calculated.")
         assertNextLine("[IMPORTANT] Simulation Statistics: Farm 1 collected 1083000 g of harvest.")
     }
