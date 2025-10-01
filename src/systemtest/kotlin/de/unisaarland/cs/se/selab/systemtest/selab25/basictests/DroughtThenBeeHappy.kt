33b4b24ca3c0c80c9f1f353597ba1250c2b034da
Kerem <kekx00001@stud.uni-saarland.de>
2025-10-01 11:46:47 +0200

    droughtthenbeehappy SystemTestRegistration.kt

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/DroughtThenBeeHappy.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/DroughtThenBeeHappy.kt
@@ -1,8 +1,6 @@
 package de.unisaarland.cs.se.selab.systemtest.selab25.basictests
 
 import de.unisaarland.cs.se.selab.systemtest.selab25.utils.ExampleSystemTestExtension
-import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogLevel
-import de.unisaarland.cs.se.selab.systemtest.selab25.utils.LogType
 
 /**
  * drought followed by a bee happy on the same tile in the same tick
@@ -20,14 +18,38 @@ class DroughtThenBeeHappy : ExampleSystemTestExtension() {
     override val maxTicks = 1
     override val startYearTick = 4
 
+    val lines = """
+        [INFO] Initialization Info: map.json successfully parsed and validated.
+        [INFO] Initialization Info: farms.json successfully parsed and validated.
+        [INFO] Initialization Info: scenario.json successfully parsed and validated.
+        [INFO] Simulation Info: Simulation started at tick 4 within the year.
+        [INFO] Simulation Info: Tick 0 started at tick 4 within the year.
+        [INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 0 PLANTATION tiles.
+        [IMPORTANT] Farm: Farm 0 starts its actions.
+        [DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: .
+        [IMPORTANT] Farm: Farm 0 finished its actions.
+        [IMPORTANT] Incident: Incident 0 of type DROUGHT happened and affected tiles 1.
+        [IMPORTANT] Incident: Incident 1 of type BEE_HAPPY happened and affected tiles 1.
+        [INFO] Harvest Estimate: Harvest estimate on tile 1 changed to 0 g of ALMOND.
+        [IMPORTANT] Simulation Info: Simulation ended at tick 1.
+        [IMPORTANT] Simulation Info: Simulation statistics are calculated.
+        [IMPORTANT] Simulation Statistics: Farm 0 collected 0 g of harvest.
+        [IMPORTANT] Simulation Statistics: Total amount of POTATO harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of WHEAT harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of OAT harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of PUMPKIN harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of APPLE harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of GRAPE harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of ALMOND harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total amount of CHERRY harvested: 0 g.
+        [IMPORTANT] Simulation Statistics: Total harvest estimate still in fields and plantations: 0 g.
+    """.trimIndent()
+
     override suspend fun run() {
-        val expectedLine = "[IMPORTANT] Simulation Statistics: Farm 0 collected 0 g of harvest."
-        assert(skipUntilLogType(LogLevel.IMPORTANT, LogType.SIMULATION_STATISTICS) == expectedLine)
-        listOf("POTATO", "WHEAT", "OAT", "PUMPKIN", "APPLE", "GRAPE", "ALMOND", "CHERRY").forEach { plant ->
-            assertNextLine("[IMPORTANT] Simulation Statistics: Total amount of $plant harvested: 0 g.")
+        val lineIterator = lines.lines().iterator()
+        while (lineIterator.hasNext()) {
+            val currentLine = lineIterator.next()
+            assertNextLine(currentLine)
         }
-        assertNextLine(
-            "[IMPORTANT] Simulation Statistics: Total harvest estimate still in fields and plantations: 157504 g."
-        )
     }
 }
