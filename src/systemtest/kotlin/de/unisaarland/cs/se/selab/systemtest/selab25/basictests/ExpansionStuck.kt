35a3613c82aeb73567ca3fc1339bec060368ce7e
Dipu <diba00008@stud.uni-saarland.de>
2025-10-02 03:37:52 +0200

    cityExpansion and machine stuck test fix

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/ExpansionStuck.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/ExpansionStuck.kt
@@ -14,12 +14,25 @@ class ExpansionStuck : ExampleSystemTestExtension() {
     override val scenario = "ExpansionStuck/scenario.json"
     override val map = "ExpansionStuck/map.json"
 
-    override val logLevel = "DEBUG"
+    override val logLevel = "IMPORTANT"
     override val maxTicks = 3
     override val startYearTick = 16
 
     val hi = """
-
+[IMPORTANT] Farm: Farm 0 starts its actions.
+[IMPORTANT] Farm: Farm 0 finished its actions.
+[IMPORTANT] Incident: Incident 0 of type CITY_EXPANSION happened and affected tiles 1.
+[IMPORTANT] Farm: Farm 0 starts its actions.
+[IMPORTANT] Farm Action: Machine 0 performs HARVESTING on tile 2 for 4 days.
+[IMPORTANT] Farm Harvest: Machine 0 has collected 1115370 g of APPLE harvest.
+[IMPORTANT] Farm Machine: Machine 0 is finished but failed to return.
+[IMPORTANT] Farm: Farm 0 finished its actions.
+[IMPORTANT] Incident: Incident 1 of type BROKEN_MACHINE happened and affected tiles 2.
+[IMPORTANT] Farm: Farm 0 starts its actions.
+[IMPORTANT] Farm: Farm 0 finished its actions.
+[IMPORTANT] Simulation Info: Simulation ended at tick 3.
+[IMPORTANT] Simulation Info: Simulation statistics are calculated.
+[IMPORTANT] Simulation Statistics: Farm 0 collected 1115370 g of harvest.
     """.trimIndent()
 
     override suspend fun run() {
