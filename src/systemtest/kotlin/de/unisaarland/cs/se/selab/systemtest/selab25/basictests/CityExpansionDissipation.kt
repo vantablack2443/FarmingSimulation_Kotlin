7502e9b11b7dbd47b3d831f14d9f03b9d2f18c99
Kerem <kekx00001@stud.uni-saarland.de>
2025-10-02 04:53:15 +0200

    city expansion dissipation registration 2

--- src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/CityExpansionDissipation.kt
+++ src/systemtest/kotlin/de/unisaarland/cs/se/selab/systemtest/selab25/basictests/CityExpansionDissipation.kt
@@ -28,7 +28,33 @@ class CityExpansionDissipation : ExampleSystemTestExtension() {
 
     private fun result(): String {
         return """
-            asdasd
+[INFO] Initialization Info: map.json successfully parsed and validated.
+[INFO] Initialization Info: farms.json successfully parsed and validated.
+[INFO] Initialization Info: scenario.json successfully parsed and validated.
+[INFO] Simulation Info: Simulation started at tick 1 within the year.
+[INFO] Simulation Info: Tick 0 started at tick 1 within the year.
+[INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 0 PLANTATION tiles.
+[IMPORTANT] Farm: Farm 0 starts its actions.
+[DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: .
+[IMPORTANT] Farm: Farm 0 finished its actions.
+[IMPORTANT] Incident: Incident 0 of type CITY_EXPANSION happened and affected tiles 1.
+[INFO] Simulation Info: Tick 1 started at tick 2 within the year.
+[INFO] Soil Moisture: The soil moisture is below threshold in 0 FIELD and 0 PLANTATION tiles.
+[IMPORTANT] Farm: Farm 0 starts its actions.
+[DEBUG] Farm: Farm 0 has the following active sowing plans it intends to pursue in this tick: .
+[IMPORTANT] Farm: Farm 0 finished its actions.
+[IMPORTANT] Simulation Info: Simulation ended at tick 2.
+[IMPORTANT] Simulation Info: Simulation statistics are calculated.
+[IMPORTANT] Simulation Statistics: Farm 0 collected 0 g of harvest.
+[IMPORTANT] Simulation Statistics: Total amount of POTATO harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of WHEAT harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of OAT harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of PUMPKIN harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of APPLE harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of GRAPE harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of ALMOND harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total amount of CHERRY harvested: 0 g.
+[IMPORTANT] Simulation Statistics: Total harvest estimate still in fields and plantations: 800000 g.
         """.trimIndent()
     }
 }
